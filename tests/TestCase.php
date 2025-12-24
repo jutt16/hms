c16a301da072b67a68afae923213108a92b3eab1
jutt16 <shahidfaqeer02@gmail.com>
2025-12-24 16:16:45 +0500

    v1

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Vite;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // In test environment, if manifest doesn't exist, skip Vite
+        // This allows tests to run even if assets aren't built
+        if (! file_exists(public_path('build/manifest.json'))) {
+            Vite::withoutVite();
+        }
+    }
 }
